668bda2d8b90723dbe6fa6e17f754289cff00a90
Marie Svanes <marie.andreassen.svanes@nhn.no>
2025-03-14 14:24:47 +0100

    feat: legge til eti-designtokens

---
+++ design-tokens-build/colors.d.ts
@@ -0,0 +1,14 @@
+import type {} from '@digdir/designsystemet-react/colors';
+
+declare module '@digdir/designsystemet-react/colors' {
+  export interface MainAndSupportColors {
+    primary: never;
+    secondary: never;
+    accent: never;
+    teal: never;
+    peach: never;
+    turqouise: never;
+    pink: never;
+    forrest: never;
+  }
+}
